01f5869472a2cd3508a0ba2e2b8bfbd47cae42de
Ben Hale <bhale@gopivotal.com>
2014-04-04 16:39:36 +0100

    Safe Tomcat download location
    Previously the download location for Tomcat (from a mirrored location)
    was unsafe.  When new versions of Tomcat were released, they old links
    expired and builds would break.  This change updates the location of
    the downloads to be from a permanent location.
    
    This change also updates the Travis configuration to work against the
    latest Tomcat 7 and 8.

--- rake/tomcat_rake_task.rb
+++ rake/tomcat_rake_task.rb
@@ -38,7 +38,7 @@ class TomcatRakeTask < Rake::TaskLib
   private
 
   def url
-    "http://mirror.cc.columbia.edu/pub/software/apache/tomcat/tomcat-#{@version.chars.first}/v#{@version}/bin" \
+    "http://archive.apache.org/dist/tomcat/tomcat-#{@version.chars.first}/v#{@version}/bin" \
     "/apache-tomcat-#{@version}.tar.gz"
   end
 
